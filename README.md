5ebf86e2289f11ea45a9e79bab8179fb85571a39
Shabir Nawabi <Shabir.n@gmail.com>
2023-03-21 06:44:35 -0700

    Final

--- README.md
+++ README.md
@@ -11,5 +11,14 @@
 ## Lessons Learned
 
 
+  -The importance of being aware of rounding errors when type casting.
+  
+  -More than likely any adversity I face in my Java adventure has been faced and overcome by others.  Solutions and strategies 
+  are available if I learn my search terms and correct terminology,
+  
+  -I learned that it's not a good Idea to be in a car during class hours while setting up for a homework assignment.   
+  I learned its easy to over extended myself while over estimated my abilities.   To be humble and and honest
+  with the staff, rather than trying to be sneaky,  I should be utilizing the instructors to enhance my learning and have a cooperative approach. 
+  Instead of an adversarial mindset.
 
 
--- src/MakeChange.java
+++ src/MakeChange.java
@@ -28,33 +28,57 @@ public class MakeChange {
 		
         long changeMultiplied = Math.round(changeAmount*100);
         
-         amount = changeMultiplied;
+        amount = changeMultiplied;
         
         twenty = amount / 2000;
         amount = amount % 2000;
+        if (twenty > 0 ) {
+        	System.out.println(twenty + " Twenty dollar bill");
+        }
+        
         ten = amount / 1000;
         amount = amount % 1000;
-        five = amount / 500;
+        if (ten > 0 ) {
+        	System.out.println(ten + " Ten dollar bill");
+        }
+        
+        five = amount / 500;        
         amount = amount % 500;
-        singles = amount / 100;
-        amount = amount % 100;		
+        if (five > 0 ) {
+        	System.out.println(five + " Five dollar bill");
+        }
+      
+        
+        singles = amount / 100;    
+        amount = amount % 100;	
+        if (singles > 0 ) {
+        	System.out.println(singles + "  dollar bill");
+        }
+        
         quarters = amount / 25;
         amount = amount % 25;
+        if (quarters > 0 ) {
+        	System.out.println(quarters + " quarters");
+        }
+        
         dimes = amount / 10;
         amount = amount % 10;
+        if (dimes> 0 ) {
+        	System.out.println(dimes + " dimes");
+        }
+       
         nickels = amount / 5;
         amount = amount % 5;
+        if (nickels > 0 ) {
+        	System.out.println(nickels + " nickels");
+        }
+        
         pennies = amount;
-                 
-                 System.out.println(twenty);
-                 System.out.println(ten);
-                 System.out.println(five);
-                 System.out.println(singles);
-                		                           
-                		        System.out.println(quarters + " quarters");
-                		         System.out.println(dimes + " dimes");
-                		         System.out.println(nickels + " nickels and");
-                		        System.out.println(pennies + " pennies");
+        if (pennies > 0 ) {
+        	System.out.println(pennies + "  pennies");
+        }
+                
+ 
 		
                  
 	
